2366fd6bbe0d2261690e7bc1a89a399f00ae4ba4
Dave Lemphers <dave@mycodepilot.com>
2019-03-03 19:22:26 -0600

    Added cors

--- mule/app/app.ts
+++ mule/app/app.ts
@@ -2,12 +2,16 @@
 import express from "express";
 import expressWs from "express-ws";
 
+import cors from "cors";
+
 import { environment } from './environment';
 
 import { ping } from './handlers/ping';
 
 // Create a new express application instance
 const app = expressWs(express()).app;
+
+app.use(cors())
 
 app.get('/ping', ping);
 
